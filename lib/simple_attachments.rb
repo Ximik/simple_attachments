edc2c16ad73d8d4f646c4804e8ce1ebe905ba893
Alex Tsokurov <me@ximik.net>
2012-02-03 23:42:31 +0400

    locales_fix

--- lib/simple_attachments.rb
+++ lib/simple_attachments.rb
@@ -2,6 +2,7 @@ module SimpleAttachments
   class Engine < ::Rails::Engine
   end
 end
+I18n.load_path << File.join(File.dirname(__FILE__), '..', 'locales', 'en.yml')
 
 require 'simple_attachments/attachment_model'
 require 'simple_attachments/container_model'
--- test/simple_attachments_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SimpleAttachmentsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
