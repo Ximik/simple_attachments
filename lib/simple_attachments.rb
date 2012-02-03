d8b460ee34dd82b05a8af7f4842b1c45196722a6
Ximik <me@ximik.net>
2012-02-03 21:36:44 +0400

    q

--- lib/simple_attachments.rb
+++ lib/simple_attachments.rb
@@ -3,5 +3,5 @@ end
 
 require 'simple_attachments/attachment_model'
 require 'simple_attachments/container_model'
-require 'simple_attachments/attachment_controller'
+require 'simple_attachments/attachments_controller'
 require 'simple_attachments/form_builder_helpers'
