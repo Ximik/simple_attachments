8e8eeadf2c032402bd90abb3abd839db5f141164
Alex Tsokurov <me@ximik.net>
2012-02-04 18:44:32 +0400

    fix

--- lib/simple_attachments/container_model.rb
+++ lib/simple_attachments/container_model.rb
@@ -3,14 +3,14 @@ module SimpleAttachments::ContainerModel
     if options[:with] == :simple_attachments
       self.class.send(:attr_accessor, :attachments)
       self.class.send(:attr_accessor, :attachment_model)
+      self.attachments = name.to_s
+      self.attachment_model = self.attachments.classify.constantize
       send(:define_method, self.attachments + '=') do |attachment_ids|
         attachment_ids.each do |attachment_id|
           attachment = self.class.attachment_model.find_by_id attachment_id
           send(self.class.attachments).push(attachment) unless attachment.nil?
         end
       end
-      self.attachments = name.to_s
-      self.attachment_model = self.attachments.classify.constantize
       options.delete :with
     end
     super
@@ -19,12 +19,12 @@ module SimpleAttachments::ContainerModel
     if options[:with] == :simple_attachments
       self.class.send(:attr_accessor, :attachment)
       self.class.send(:attr_accessor, :attachment_model)
+      self.attachment = name.to_s
+      self.attachment_model = self.attachment.classify.constantize
       send(:define_method, self.attachment + '=') do |attachment_ids|
         attachment = self.class.attachment_model.find_by_id attachment_id
         send(self.class.attachment + '=', attachment) unless attachment.nil?
       end
-      self.attachment = name.to_s
-      self.attachment_model = self.attachment.classify.constantize
       options.delete :with
     end
     super
