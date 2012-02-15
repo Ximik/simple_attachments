244d837ca32e3e2d43d9a8076ce1db346444ec15
Alex Tsokurov <me@ximik.net>
2012-02-15 20:51:20 +0000

    helpers

--- lib/simple_attachments.rb
+++ lib/simple_attachments.rb
@@ -7,4 +7,4 @@ I18n.load_path << File.join(File.dirname(__FILE__), '..', 'locales', 'en.yml')
 require 'simple_attachments/attachment_model'
 require 'simple_attachments/container_model'
 require 'simple_attachments/attachments_controller'
-require 'simple_attachments/form_builder_helpers'
+require 'simple_attachments/helpers'
